978069984048ec5cd7b8a59e830081a633fab0d0
Chuyi Qian <don.capa@gmail.com>
2018-03-19 20:47:58 +0000

    BaiduOldDriver Commit

---
+++ BaiduOldDriver/BaiduOldDriver/Client/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NetDisk;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Client
+{
+    public partial class MainForm : Form
+    {
+        private const string DirLabel = "<文件夹>";
+        private Credential cred;
+        private string path = "";
+        public MainForm(Credential cred)
+        {
+            this.cred = cred;
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoadFileList();
+        }
+        private void LoadFileList()
+        {
+            listView1.Items.Clear();
+            listView1.Enabled = false;
+            toolStripButton1.Enabled = false;
+            toolStripLabel1.Text = path == "" ? "/" : path;
+            Application.DoEvents();
+            var res = Operation.GetFileList("/" + path, cred);
+            if (res.success == false || res.errno != 0)
+            {
+                MessageBox.Show("Error: " + res.exception);
+                path = "";
+            }
+            else
+            {
+                if (path != "") listView1.Items.Add(new ListViewItem(new[] { "", ".." }));
+                foreach (var item in res.list)
+                {
+                    listView1.Items.Add(new ListViewItem(new[] { "", item.server_filename, item.isdir == 0 ? BytesToString(item.size) : DirLabel }));
+                }
+            }
+            listView1.Enabled = true;
+            toolStripButton1.Enabled = true;
+        }
+        private static string BytesToString(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (byteCount == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            LoadFileList();
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0)
+            {
+                var line = listView1.SelectedItems[0];
+                if (line.SubItems.Count == 2 || line.SubItems[2].Text == DirLabel)
+                {
+                    if (line.SubItems[1].Text == "..")
+                        path = path.Substring(0, path.LastIndexOf('/'));
+                    else
+                        path += "/" + line.SubItems[1].Text;
+                    LoadFileList();
+                }
+                else
+                {
+                    listView1.Enabled = false;
+                    toolStripButton1.Enabled = false;
+                    Application.DoEvents();
+                    var pwd = GeneratePwd();
+                    var sres = Operation.Share(new[] { path + "/" + line.SubItems[1].Text }, cred, pwd);
+                    try
+                    {
+                        if (sres.success == false || sres.errno != 0) throw sres.exception;
+                        using (var wc = new WebClient())
+                        {
+                            var str = wc.DownloadString("http://stomakun.me:9999/" + Uri.EscapeDataString(sres.link) + "/" + pwd);
+                            if (str.Contains("Error:") || !str.Contains("http://")) throw new Exception(str);
+                            ShowLinks(str);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    listView1.Enabled = true;
+                    toolStripButton1.Enabled = true;
+                }
+            }
+        }
+        private void ShowLinks(string str)
+        {
+            using(var sw=new StreamWriter("link.html"))
+            {
+                var html = "";
+                var i = 1;
+                var del = "\n";
+                if (str.Contains("\r\n")) del = "\r\n";
+                else if (str.Contains("\r")) del = "\r";
+                foreach (var item in str.Split(new[] { del }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var match = Regex.Match(item, "https?:\\/\\/(.+?)\\/");
+                    html += "<p><a href=\"" + item + "\">下载地址" + i + (match.Success ? "&nbsp;(" + match.Groups[1].Value + ")" : "") + "</a></p>";
+                    i++;
+                }
+                sw.WriteLine(html);
+            }
+            System.Diagnostics.Process.Start("link.html");
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+        private string GeneratePwd()
+        {
+            var sb = new StringBuilder();
+            var rand = new Random();
+            for(int i = 0; i < 4; i++)
+            {
+                var type = rand.Next(3);
+                if (type == 0) sb.Append((char)('0' + rand.Next(10)));
+                else if (type == 1) sb.Append((char)('a' + rand.Next(26)));
+                else if (type == 2) sb.Append((char)('A' + rand.Next(26)));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ BaiduOldDriver/BaiduOldDriver/Test/Test.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Linq;
+using NetDisk;
+using Downloader;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Test
+{
+    public class Test
+    {
+        static void CheckSuccess(Result result)
+        {
+            if (!result.success)
+            {
+                Console.WriteLine(result.exception);
+                Console.ReadLine();
+                Environment.Exit(-1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            var username = "伪红学家";
+            var password = "******";
+            // Test login check
+            var checkResult = Authentication.LoginCheck(username);
+            CheckSuccess(checkResult);
+            // Handle verify code
+            if (checkResult.needVCode)
+            {
+                File.WriteAllBytes("vcode.png", checkResult.image);
+                Console.WriteLine("Verification code required. Input the text in vcode.png.");
+                try
+                {
+                    System.Diagnostics.Process.Start("vcode.png");
+                }
+                catch (Exception) { }
+                checkResult.verifyCode = Console.ReadLine();
+                try
+                {
+                    File.Delete("vcode.png");
+                }
+                catch (Exception) { }
+            }
+            else Console.WriteLine("Verification code NOT required.");
+            // Test login
+            var loginResult = Authentication.Login(username, password, checkResult);
+            CheckSuccess(loginResult);
+            Console.WriteLine(loginResult.credential);
+            Console.WriteLine("uid: " + loginResult.credential.uid);
+            var credential = loginResult.credential;
+            // Test get quota
+            var quotaResult = Operation.GetQuota(credential);
+            CheckSuccess(quotaResult);
+            Console.WriteLine(quotaResult.used + "/" + quotaResult.total);
+            // Test get user info
+            var infoResult = Operation.GetUserInfo(credential);
+            CheckSuccess(infoResult);
+            Console.WriteLine(infoResult.records[0].uname + " " + infoResult.records[0].priority_name + " " + infoResult.records[0].avatar_url);
+            // Test list file
+            var fileListResult = Operation.GetFileList("/", credential);
+            CheckSuccess(fileListResult);
+            Console.WriteLine(string.Join("\r\n", fileListResult.list.Take(5).Select(e => e.path + " " + e.isdir + " " + e.size).ToArray()));
+            // Test thumbnail
+            var thumbnailResult = Operation.GetThumbnail("/1.mp4", credential);
+            CheckSuccess(thumbnailResult);
+            Console.WriteLine("Thumbnail " + thumbnailResult.image.Length + " bytes.");
+            try
+            {
+                File.WriteAllBytes("thumb.jpg", thumbnailResult.image);
+                var process = System.Diagnostics.Process.Start("thumb.jpg");
+            }
+            catch (Exception) { }
+            // Test rapid upload
+            var rapidUploadResult = Operation.RapidUpload(UploadHelper.GetFileProperty("Z:\\Thunder9.0.14.358.exe"), "/t.exe", credential);
+            CheckSuccess(rapidUploadResult);
+            Console.WriteLine("Rapid: " + rapidUploadResult.info.path + " " + rapidUploadResult.info.size);
+            // Test get upload servers
+            var servers = Operation.GetUploadServers(credential);
+            CheckSuccess(servers);
+            Console.WriteLine("Servers: " + string.Join(" ", servers.servers));
+            // Test simple upload
+            var simpleUploadResult = Operation.SimpleUpload("Z:\\1.rar", "/1.rar", credential, servers.servers[0]);
+            CheckSuccess(simpleUploadResult);
+            Console.WriteLine("Simple: " + simpleUploadResult.path + " " + simpleUploadResult.size);
+            // Test chunked upload
+            var chunkedUploadResult = Operation.ChunkedUpload("Z:\\2.rar", "/2.rar", credential);
+            CheckSuccess(chunkedUploadResult);
+            Console.WriteLine("Chunked: " + chunkedUploadResult.path + " " + chunkedUploadResult.size);
+            // Test get download urls
+            var downloadResult = Operation.GetDownload("/1.mp4", credential);
+            CheckSuccess(downloadResult);
+            Console.WriteLine("Download URL count = " + downloadResult.urls.Length + " " + downloadResult.urls[0].rank + " " + downloadResult.urls[0].url);
+            // Test file operations
+            var fileopResult = Operation.Copy("/1.mp4", "/", "2.mp4", credential);
+            CheckSuccess(fileopResult);
+            fileopResult = Operation.Move("/2.mp4", "/", "3.mp4", credential);
+            CheckSuccess(fileopResult);
+            fileopResult = Operation.Rename("/3.mp4", "4.mp4", credential);
+            CheckSuccess(fileopResult);
+            fileopResult = Operation.Delete("/4.mp4", credential);
+            CheckSuccess(fileopResult);
+            fileopResult = Operation.CreateFolder("/test", credential);
+            CheckSuccess(fileopResult);
+            Console.WriteLine("New folder name: " + fileopResult.path);
+            // Test share
+            var shareResult = Operation.Share(new[] { "/1.mp4" }, credential);
+            CheckSuccess(shareResult);
+            Console.WriteLine(shareResult.link + " " + shareResult.shorturl);
+            shareResult = Operation.Share(new[] { "/1.mp4" }, credential, "8888");
+            CheckSuccess(shareResult);
+            Console.WriteLine(shareResult.link + " " + shareResult.shorturl);
+            // Test transfer shared files
+            var transferResult = Operation.Transfer("http://pan.baidu.com/s/1hsfZ1TM", "/", credential, "1w9w");
+            CheckSuccess(transferResult);
+            Console.WriteLine(transferResult.extra.list.Select(e => e.from + " " + e.to + "\r\n").ToString());
+            // Test offline
+            var queryLinkResult = Operation.QueryLinkFiles("/downfile.torrent", credential);
+            CheckSuccess(queryLinkResult);
+            queryLinkResult.files.ToList().ForEach(f => Console.WriteLine(f.file_name + " " + f.size));
+            var addOfflineResult = Operation.AddOfflineTask("/downfile.torrent", "/", credential, new[] { 1 }, queryLinkResult.sha1);
+            CheckSuccess(addOfflineResult);
+            Console.WriteLine(addOfflineResult.task_id + " " + addOfflineResult.rapid_download);
+            addOfflineResult = Operation.AddOfflineTask("http://www.baidu.com/", "/", credential);
+            CheckSuccess(addOfflineResult);
+            Console.WriteLine(addOfflineResult.task_id + " " + addOfflineResult.rapid_download);
+            addOfflineResult = Operation.AddOfflineTask("magnet:?xt=urn:btih:eb748516ee0968422d9827a9991d28cbd4dc4f3f", "/", credential, new[] { 1, 2 });
+            CheckSuccess(addOfflineResult);
+            Console.WriteLine(addOfflineResult.task_id + " " + addOfflineResult.rapid_download);
+            addOfflineResult = Operation.AddOfflineTask("ed2k://|file|[柳井正与优衣库].蔡成平.文字版(ED2000.COM).epub|890525|90a92b89df20a8b2283fe3450f872590|h=oxrt54bznbtq5pr47dfmj4zfn5ctbvek|/", "/", credential);
+            CheckSuccess(addOfflineResult);
+            Console.WriteLine(addOfflineResult.task_id + " " + addOfflineResult.rapid_download);
+            var offlineListResult = Operation.GetOfflineList(credential);
+            CheckSuccess(offlineListResult);
+            offlineListResult.tasks.ToList().ForEach(t => Console.WriteLine(t.status + " " + t.task_id + " " + t.task_name + " " + t.finished_size + "/" + t.file_size + " " + t.od_type));
+            Operation.ClearOfflineTask(credential);
+            foreach (var item in offlineListResult.tasks)
+            {
+                Operation.CancelOfflineTask(item.task_id, credential);
+                Operation.DeleteOfflineTask(item.task_id, credential);
+            }
+            offlineListResult = Operation.GetOfflineList(credential);
+            CheckSuccess(offlineListResult);
+            Debug.Assert(offlineListResult.tasks.Length == 0);
+            Console.WriteLine("Offline list cleared.");
+            // Test download
+            var adapter = new BaiduAdapter() { credential = credential, path = "/1.mp4", size = fileListResult.list.First(e => e.path == "/1.mp4").size };
+            var task = new FileTask(adapter, "Z:\\1.mp4");
+            var st = new Stopwatch();
+            st.Start();
+            while (!task.IsCompleted())
+            {
+                Console.WriteLine(task.GetDownloadedBytes());
+                foreach (var item in task.DownloadSources)
+                {
+                    Console.WriteLine("  " + item.URL.Substring(0,20) + " " + item.GetDownloadedBytes());
+                }
+                Thread.Sleep(1000);
+            }
+            st.Stop();
+            Console.WriteLine("Time taken: " + st.ElapsedMilliseconds / 1000);
+            // Done
+            Console.WriteLine("Success");
+            Console.ReadLine();
+        }
+    }
+}
